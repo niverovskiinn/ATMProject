fc8c8c4af59deaf43affd4188cfed57cd9077d5d
Never04eK <heket844@gmail.com>
2019-11-10 19:01:21 +0200

    Move DB

--- ATM/Engine/DataAccess/AtmDbContext.cs
+++ ATM/Engine/DataAccess/AtmDbContext.cs
@@ -15,75 +15,78 @@ namespace Engine.DataAccess
         
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-             List<Card> cards = new List<Card>
-            {
-                new Card
-                {
-                    Number = "1234123412341234",
-                    AccountId = 1
-                    
-                },
-                new Card
-                {
-                    Number = "1234123412341235",
-                    AccountId = 2
-                }
-            };
+            #region Add Entities to DB
+//             List<Card> cards = new List<Card>
+//            {
+//                new Card
+//                {
+//                    Number = "1234123412341234",
+//                    AccountId = 1
+//                    
+//                },
+//                new Card
+//                {
+//                    Number = "1234123412341235",
+//                    AccountId = 2
+//                }
+//            };
+//
+//             List<Account> accounts = new List<Account>
+//            {
+//                new Account
+//                {
+//                    Id = 1,
+//                    OwnerPassport = "AA123456",
+//
+//                },
+//                new Account
+//                {
+//                    Id = 2,
+//                    OwnerPassport = "AA654321",
+//                },
+//            };
+//
+//             List<User> users = new List<User>            {
+//                new User
+//                {
+//                    Passport = "AA123456",
+//                    Accounts = accounts
+//
+//                },
+//                new User
+//                {
+//                    Passport = "AA654321",
+//                    Accounts = accounts
+//                }
+//            };
+//            var transactions = new List<Transaction>
+//            {
+//                
+//                new Transaction
+//                {
+//                    Id = 1,
+//                    AccountFromId = 1,
+//                    AccountToId = 2,
+//                    From = accounts[0],
+//                    To = accounts[1]
+//
+//                },
+//                new Transaction
+//                {
+//                    Id = 2,
+//                    AccountFromId = 1,
+//                    From = accounts[0],
+//                    To = null
+//                }
+//            };
+//
+//            
+////            modelBuilder.Entity<User>().HasData(users);
+////            modelBuilder.Entity<Account>().HasData(accounts);
+////            modelBuilder.Entity<Card>().HasData(cards);
+////            modelBuilder.Entity<Transaction>().HasData(transactions);
+#endregion
 
-             List<Account> accounts = new List<Account>
-            {
-                new Account
-                {
-                    Id = 1,
-                    OwnerPassport = "AA123456",
-
-                },
-                new Account
-                {
-                    Id = 2,
-                    OwnerPassport = "AA654321",
-                },
-            };
-
-             List<User> users = new List<User>            {
-                new User
-                {
-                    Passport = "AA123456",
-                    Accounts = accounts
-
-                },
-                new User
-                {
-                    Passport = "AA654321",
-                    Accounts = accounts
-                }
-            };
-            var transactions = new List<Transaction>
-            {
-                
-                new Transaction
-                {
-                    Id = 1,
-                    AccountFromId = 1,
-                    AccountToId = 2,
-                    From = accounts[0],
-                    To = accounts[1]
-
-                },
-                new Transaction
-                {
-                    Id = 2,
-                    AccountFromId = 1,
-                    From = accounts[0],
-                    To = null
-                }
-            };
-
-            
-//            modelBuilder.Entity<User>().HasData(users);
-//            modelBuilder.Entity<Account>().HasData(accounts);
-//            modelBuilder.Entity<Card>().HasData(cards);
-//            modelBuilder.Entity<Transaction>().HasData(transactions);
             modelBuilder.Entity<AccountStatus>().HasData(AccountStatusEnum.Active, AccountStatusEnum.Closed, 
                 AccountStatusEnum.Frozen);
             modelBuilder.Entity<AccountType>().HasData(AccountTypeEnum.Credit, AccountTypeEnum.Debit);
@@ -92,11 +95,11 @@ namespace Engine.DataAccess
             base.OnModelCreating(modelBuilder);
         }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            
-            optionsBuilder.UseSqlite("Data Source=/Users/nikita/Documents/GitHub/ATMProject/ATM/DataAccess/AtmDb.db");
-        }
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            
+//            optionsBuilder.UseSqlite("Data Source=/Users/nikita/Documents/GitHub/ATMProject/ATM/Engine/DataAccess/AtmDb.db");
+//        }
 
         public DbSet<Account> Accounts { get; set; }
         public DbSet<AccountStatus> AccountStatuses { get; set; }
